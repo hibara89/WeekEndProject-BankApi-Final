e0d717c170597bf72ec5d74fd83d4b05f354e84e
hibara89 <hiba.rashed@gmail.com>
2022-06-26 13:29:50 +0300

    backend data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # WeekEndProject-BankApi-Final
+
+This is the final project in Appleseeds bootcamp.
+In this project we implement all the technologies we studied during the course:
+1.React
+2.Express
+3.Node
+4.MongoDb
--- server/server.js
+++ server/server.js
@@ -1,10 +1,38 @@
 import express from "express";
+import mongoose from "mongoose";
+
 const app = express();
 
-app.get("/api", (req, res) => {
-  res.json({ users: ["user1", "user2", "user3"] });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.get("/api", (req, res) => {
+//   res.json({ users: ["user1", "user2", "user3", "user4", "user5"] });
+// });
+
+app.get("/users", (req, res) => {
+  res.json("get all users in the bank");
 });
 
-app.listen(5000, () => {
+app.get("/users:id", (req, res) => {
+  res.json("get user with id");
+});
+
+app.post("/users:id", (req, res) => {
+  console.log(req.params);
+  res.json("add user with id");
+});
+app.delete("/users:id", (req, res) => {
+  res.json("delete user with id");
+});
+
+mongoose.connect("mongodb://localhost:27017/myBank", () => {
+  console.log("hello");
+});
+
+app.listen(5000, (error) => {
+  if (error) {
+    console.log("error");
+  }
   console.log("the server is up adm listening to port 5000");
 });
